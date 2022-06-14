567ae8ccd1c46f18a642942b0b5ee3a814cfcb46
Heena Padshala <heenagajera96@gmail.com>
2022-06-14 11:01:43 +0530

    SquareZRoot test case

--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -6,6 +6,20 @@ namespace MathTests
     public class UnitTest1
     {
         [TestMethod]
+        public void BasicRooterTest()
+        {
+            // Create an instance to test:
+            Rooter rooter = new Rooter();
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot(input);
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
         public void RooterValueRange()
         {
             // Create an instance to test.
@@ -23,6 +37,21 @@ namespace MathTests
             double input = expectedResult * expectedResult;
             double actualResult = rooter.SquareRoot(input);
             Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+        }
+
+        [TestMethod]
+        public void RooterTestNegativeInputx()
+        {
+            Rooter rooter = new Rooter();
+            try
+            {
+                rooter.SquareRoot(-10);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail();
         }
     }
 }
--- MyMath/MyMath/Class1.cs
+++ MyMath/MyMath/Class1.cs
@@ -9,12 +9,18 @@
     {
         public double SquareRoot(double input)
         {
+            if (input <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             double result = input;
             double previousResult = -input;
             while (Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
-                result = result - (result * result - input) / (2 * result);
+                result = (result + input / result) / 2;
+                //was: result = result - (result * result - input) / (2*result);
             }
             return result;
         }
